51df3239ced9f3844c21a69af6200b5562ecc6b2
Davi Reis <dreisss.dev@gmail.com>
2025-07-15 15:32:18 -0300

    feat(api): add swagger plugin

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,18 +1,28 @@
 import { cors } from '@elysiajs/cors'
+import { swagger } from '@elysiajs/swagger'
+
 import { env } from '@repo/env'
 import { Elysia } from 'elysia'
+
 import { getRooms } from '@/http/routes/get-rooms'
 
 const corsPlugin = cors({
   origin: `localhost:${env.WEB_PORT}`,
 })
 
+const swaggerPlugin = swagger({
+  path: 'docs',
+})
+
 const healthRoute = new Elysia().get('/health', 'OK')
 
 const api = new Elysia()
   .use(corsPlugin)
+  .use(swaggerPlugin)
+
   .use(healthRoute)
   .use(getRooms)
+
   .listen(env.API_PORT)
 
 console.info(
